99a60d846d562c246433d076ccc2e9376ea8f1ce
Yuanlai <869494563@qq.com>
2018-04-30 15:32:48 -0700

    add vertex and edge with name property

--- ssc.cpp
+++ ssc.cpp
@@ -3,6 +3,7 @@
 #include <boost/property_tree/json_parser.hpp>
 #include "boost/graph/graph_traits.hpp"
 #include "boost/graph/adjacency_list.hpp"
+#include <tuple>
 
 using namespace std;
 namespace pt = boost::property_tree;
@@ -10,23 +11,48 @@ namespace pt = boost::property_tree;
 int main() {
 	pt::ptree root;
 	pt::read_json("SampleDataset1/SampleDataset1.json", root);
-	typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS> UndirectedGraph;
-	typedef pair<string, string> Edge;
+	typedef tuple<string, string, string> Edge;
+	typedef boost::property<boost::vertex_name_t, std::string> VertexProperty;
+	typedef boost::property<boost::edge_name_t, std::string> EdgeProperty;
+	typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS, VertexProperty, EdgeProperty> UndirectedGraph;
 
-	std::vector<Edge> edgeVec;
+	vector<Edge> vecEdge;
+	vector<string> vecVertices;
+	unordered_set<string> vertices;
+	map<string, UndirectedGraph::vertex_descriptor> indexes;
 
-	//string type_ = root.get<string>("type");
-	//cout << type_ << endl;
+
 	for (pt::ptree::value_type &row : root.get_child("rows")) {
 		//cout << row.second.get<string>("viaGlobalId") << endl;
-		edgeVec.push_back(Edge(row.second.get<string>("fromGlobalId"),row.second.get<string>("toGlobalId")));
+		vecEdge.push_back(Edge(row.second.get<string>("fromGlobalId"),row.second.get<string>("toGlobalId"), row.second.get<string>("viaGlobalId")));
+		vertices.insert(row.second.get<string>("fromGlobalId"));
+		vertices.insert(row.second.get<string>("toGlobalId"));
 	}
 
-	UndirectedGraph g(edgeVec.size());
-	for (int i = 0; i < edgeVec.size(); ++i) {
-    	//boost::add_edge(edgeVec[i].first, edgeVec[i].second, g);
-    	cout << edgeVec[i].first << endl;
+	int numVertices = vertices.size();
+	UndirectedGraph g(numVertices);
+	vecVertices.insert(vecVertices.end(), vertices.begin(), vertices.end());
+
+	for(int i = 0; i < numVertices; i++)
+    {
+	    boost::put(boost::vertex_name_t(), g, i, vecVertices[i]); // set the property of a vertex
+	    indexes[vecVertices[i]] = boost::vertex(i, g);     // retrives the associated vertex descriptor
     }
+
+    for(int i = 0; i < vecEdge.size(); i++)
+  	{
+    	boost::add_edge(indexes[get<0>(vecEdge[i])], indexes[get<1>(vecEdge[i])], EdgeProperty(get<2>(vecEdge[i])), g);
+  	}
+
+
+	vector<string> :: iterator itr;
+    for (itr = vecVertices.begin(); itr != vecVertices.end(); itr++)
+        cout << (*itr) << endl;
+    
+	// for (int i = 0; i < vecEdge.size(); ++i) {
+ //    	//boost::add_edge(vecEdge[i].first, vecEdge[i].second, g);
+ //    	cout << vecEdge[i].first << endl;
+ //    }
 
 
 
